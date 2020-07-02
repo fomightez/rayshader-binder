b9ce84231fdc430b96d8343162229261b681177a
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2020-07-02 15:39:26 -0400

    change rayshader install to be from CRAN

--- install.R
+++ install.R
@@ -8,4 +8,6 @@ install.packages("rgdal")
 install.packages("raster")
 install.packages("rgl")
 devtools::install_github("ropenscilabs/bindertools") # Don't need this but want to test devtools installation; install based on https://docs.ropensci.org/bindertools/
-devtools::install_github("tylermorganwall/rayshader")
+#devtools::install_github("tylermorganwall/rayshader")
+# trying install from CRAN instead of github based on https://github.com/tylermorganwall/rayshader/issues/110#issuecomment-627080365
+install.packages("rayshader") 
