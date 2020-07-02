52662e98d142edf2a29040ca620662f3fc767a37
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2020-07-02 15:55:07 -0400

    add other dependencies that say they are failing when trying to install via devtools

--- install.R
+++ install.R
@@ -8,6 +8,9 @@ install.packages("rgdal")
 install.packages("raster")
 install.packages("rgl")
 devtools::install_github("ropenscilabs/bindertools") # Don't need this but want to test devtools installation; install based on https://docs.ropensci.org/bindertools/
-#devtools::install_github("tylermorganwall/rayshader")
+devtools::install_github("tylermorganwall/terrainmeshr")
+install.packages("remotes")
+remotes::install_github("tylermorganwall/rayimage")
+devtools::install_github("tylermorganwall/rayshader")
 # trying install from CRAN instead of github based on https://github.com/tylermorganwall/rayshader/issues/110#issuecomment-627080365
-install.packages("rayshader") 
+#install.packages("rayshader") 
